af0f80d1370558df05a2bf03cbe13711c04df51d
JiaweiChenC <jc5667@columbia.edu>
2022-03-02 18:10:02 -0500

    first commit

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("/Users/jiawei/Desktop/course_note/coursera/note.org" "/Users/jiawei/Desktop/research/Pataka_Test/Oxford_foot_model_detail/OFM.org" "/Users/jiawei/Desktop/research/research_note.org"))
+ '(package-selected-packages '(flycheck-pycheckers auctex)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
